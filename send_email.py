0f546b147c935a61146b6e4a49b2032fe9cb76a1
ShrutiGupta37 <shrutigupta.cse37@gmail.com>
2025-06-10 23:52:50 +0530

    add send_email

---
+++ send_email.py
@@ -0,0 +1,24 @@
+import os
+import smtplib
+from email.message import EmailMessage
+import ssl
+
+email_sender ='finalyearprojectemail419@gmail.com'
+email_password='uiic ukco mpfi zrdx'
+email_receiver ='itzclout.exe@gmail.com'
+
+subject= "Testing for Automatic email"
+body = """
+    This mail is sent for tesing purpose.
+"""
+em = EmailMessage()
+em['From']=email_sender
+em['To']=email_receiver
+em['subject']=subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+    smtp.login(email_sender,email_password)
+    smtp.sendmail(email_sender,email_receiver,em.as_string())
+
