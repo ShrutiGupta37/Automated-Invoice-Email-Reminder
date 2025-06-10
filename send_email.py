cecad2dd92cb942d9b67a9fd42682092541a4f0f
ShrutiGupta37 <shrutigupta.cse37@gmail.com>
2025-06-11 01:33:51 +0530

    add update

--- send_email.py
+++ send_email.py
@@ -1,24 +1,35 @@
-import os
 import smtplib
 from email.message import EmailMessage
 import ssl
+import time
 
-email_sender ='finalyearprojectemail419@gmail.com'
-email_password='uiic ukco mpfi zrdx'
-email_receiver ='itzclout.exe@gmail.com'
+import pandas as pd
+df = pd.read_csv('C:\Git Demo\Email-Automation\data.csv')
+
+email_sender =' ' #add sender's email
+email_password=' ' #add 16 digit password
 
 subject= "Testing for Automatic email"
 body = """
     This mail is sent for tesing purpose.
 """
-em = EmailMessage()
-em['From']=email_sender
-em['To']=email_receiver
-em['subject']=subject
-em.set_content(body)
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
-    smtp.login(email_sender,email_password)
-    smtp.sendmail(email_sender,email_receiver,em.as_string())
+def send_mail( ):
+    for email in df['Email']:
+        email_receiver = email
+        em = EmailMessage()
+        em['From']=email_sender
+        em['To']=email_receiver
+        em['subject']=subject
+        em.set_content(body)
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+            smtp.login(email_sender,email_password)
+            smtp.sendmail(email_sender,email_receiver,em.as_string())
+
+        time.sleep(5)
+
+send_mail()
+print("All mails sent successfully")
 
